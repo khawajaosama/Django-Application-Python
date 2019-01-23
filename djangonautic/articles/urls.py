ca9f69359b2805ca0bb004864b52931aa8dfbab1
Khawaja Osama <khawajaosama007@gmail.com>
2019-01-23 14:43:41 +0500

    Named Urls

--- djangonautic/articles/urls.py
+++ djangonautic/articles/urls.py
@@ -3,10 +3,12 @@ from django.urls import path
 from django.conf.urls import url
 from . import views
 
+app_name = 'articles'  # we are name spacing this file
+
 urlpatterns = [
-    url(r'^articles_list/',views.article_list),
-    url(r'^(?P<slug>[\w-]+)/$',views.article_detail)
-    # path(r'admin/', admin.site.urls),
+    url(r'^articles_list/',views.article_list,name='list'),
+    url(r'^slug:(?P<slug>[\w-]+)/$',views.article_detail,name='detail')
+    # path(r'^admin/', admin.site.urls),
     # path(r'about/',views.about),
     #path('^$',views.about)
 ]
